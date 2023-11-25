7db5049a522fdcc830bc2f542dd3366894008e6c
Sadhu2005 <sadhuj2005@gmail.com>
2023-11-25 19:09:01 +0530

    I greated a sudoku_solver.py file

---
+++ sudoku_solver.py
@@ -0,0 +1,53 @@
+def is_valid(board, row, col, num):
+    # Check if the number already exists in the row
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+        
+    # Check if the number already exists in the column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    # Check if the number already exists in the 3x3 grid
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+def print_board(board):
+    for row in range(9):
+        for col in range(9):
+            print(board[row][col], end=" ")
+        print()
+# Example Sudoku board (0 represents empty cells)
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+if solve_sudoku(board):
+    print("Sudoku solved:")
+    print_board(board)
+else:
+    print("No solution exists for the given Sudoku board.")
+    
