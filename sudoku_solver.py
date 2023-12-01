fe403a967c1c996b4c95ebdf8620be8739188a78
Sadhu2005 <sadhuj2005@gmail.com>
2023-12-01 19:18:57 +0530

    user input added

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,7 +3,6 @@ def is_valid(board, row, col, num):
     for i in range(9):
         if board[row][i] == num:
             return False
-        
     # Check if the number already exists in the column
     for i in range(9):
         if board[i][col] == num:
@@ -34,7 +33,7 @@ def print_board(board):
             print(board[row][col], end=" ")
         print()
 # Example Sudoku board (0 represents empty cells)
-"""board = [
+board = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
     [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -44,40 +43,9 @@ def print_board(board):
     [0, 6, 0, 0, 0, 0, 2, 8, 0],
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]"""
-#above the comment when i give the example board for testing
-#now i want to take in put from the user ....-sadhu 
-def create_empty_sudoku_board():
-    return [[0 for _ in range(9)] for _ in range(9)]
-
-def print_sudoku_board(board):
-    for row in board:
-        print(" ".join(map(str, row)))
-
-def get_user_input():
-    print("Enter the Sudoku board row by row (use '0' for empty cells):")
-    board = []
-    for i in range(9):
-        while True:
-            row_input = input(f"Enter row {i + 1} (9 numbers separated by spaces): ")
-            numbers = row_input.split()
-            if len(numbers) == 9 and all(num.isdigit() and 0 <= int(num) <= 9 for num in numbers):
-                board.append([int(num) for num in numbers])
-                break
-            else:
-                print("Invalid input. Please enter 9 numbers between 0 and 9.")
-    return board
-
-# Get user input to fill the Sudoku board
-sudoku_board = get_user_input()
-
-# Display the entered Sudoku board
-print("\nThe entered Sudoku board is:")
-print_sudoku_board(sudoku_board)
-
+]
 if solve_sudoku(board):
     print("Sudoku solved:")
     print_board(board)
 else:
     print("No solution exists for the given Sudoku board.")
-    
